ea7a90afb32e1abccc1bcba6ac388d6253e82e77
VijayaMeghana <vijayameghana1116@gmail.com>
2017-11-20 20:43:06 -0600

    added main class in third class

--- src/main/java/WebApplication/WebApplication/ThirdClass.java
+++ src/main/java/WebApplication/WebApplication/ThirdClass.java
@@ -1,5 +1,13 @@
 package WebApplication.WebApplication;
-
+/**
+ * 
+ * @author vijaya meghana
+ *
+ */
 public class ThirdClass {
+	public static void main(String []args)
+	{
+		System.out.println("This is third class");
+	}
 
 }
