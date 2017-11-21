6731d11e292ddfb5023a811fef7c1cf0ac1d0a4d
sivak <skraja0590@gmail.com>
2017-11-20 21:45:23 -0500

    constructor added

--- src/main/java/WebApplication/WebApplication/FirstClass.java
+++ src/main/java/WebApplication/WebApplication/FirstClass.java
@@ -8,4 +8,11 @@ package WebApplication.WebApplication;
  */
 public class FirstClass {
 
+	/**
+	 * default constructor
+	 */
+	public FirstClass() {
+		// TODO Auto-generated constructor stub
+		System.out.println("hello world! this is chitti");
+	}
 }
--- src/main/java/WebApplication/WebApplication/SecondClass.java
+++ src/main/java/WebApplication/WebApplication/SecondClass.java
@@ -13,4 +13,5 @@ public class SecondClass {
 	public SecondClass() {
 
 	}
+
 }
