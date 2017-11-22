7a3272b09e420182cca8bf6f63c8e68ce1a69c06
VijayaMeghana <vijayameghana1116@gmail.com>
2017-11-22 11:06:06 -0600

    committing LoginServlet

---
+++ src/main/java/com/webpps/Service/LoginServlet.java
@@ -0,0 +1,13 @@
+package com.webpps.Service;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * class LoginServlet.
+ * 
+ * @author vijaya meghana
+ *
+ */
+public class LoginServlet extends HttpServlet {
+
+}
